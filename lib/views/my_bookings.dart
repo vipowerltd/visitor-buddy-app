490b0e555268abe7183fac5fa5b5ec4e83b6b2e7
Harry <freemirvm@gmail.com>
2023-03-21 17:19:48 +0000

    Fixed booking list previous bookings search functionality

--- lib/views/my_bookings.dart
+++ lib/views/my_bookings.dart
@@ -200,7 +200,7 @@ class _MyBookingsState extends State<MyBookings> {
 
   Widget previousBookings() {
     List<Widget> widgetList = [];
-    for (MeetingRoomBooking booking in bookings) {
+    for (MeetingRoomBooking booking in searchResults) {
       if (booking.end_time.isBefore(DateTime.now())) {
         widgetList.add(prevBookingsCard(booking));
       }
